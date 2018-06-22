9d6274d655c737de20c3308d04f192fa147fe876
KYKAROS\Kykaros <panadonoop3@gmail.com>
2018-06-22 22:56:36 +0700

    [MOD] MButton

--- Asset/Scripts/ManagerButton.cs
+++ Asset/Scripts/ManagerButton.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class ManagerButton : MonoBehaviour {
     bool IsClick = false;
     public static int Select = 1;
+    public GameObject TextPause;    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            BackButton();
+    }
     public void ChangeScene(string Name)
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(Name);
+        Manager.FrameGameOver = false;
+        Bird.Death = false;
     }
     public void Quit()
     {
@@ -18,19 +27,33 @@ public class ManagerButton : MonoBehaviour {
     }
     public void Pause()
     {
-        if (IsClick == false)
+        if(Bird.Death == false && Manager.FrameGameOver == false)
         {
-            Time.timeScale = 0;
-            IsClick = true;
-        }
-        else
-        {
-            Time.timeScale = 1;
-            IsClick = false;
+            if (IsClick == false)
+            {
+                Time.timeScale = 0;
+                TextPause.SetActive(true);
+                IsClick = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                TextPause.SetActive(false);
+                IsClick = false;
+            }
         }
     }
     public void SelectBird(int number)
     {
         Select = number;
+    }
+    void BackButton()
+    {
+        Application.Quit();
+    }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene(1);
     }
 }
