bbe6d52567f386c211f2e58442d4cb1e7a4cf53f
KYKAROS\Kykaros <panadonoop3@gmail.com>
2018-06-22 22:48:51 +0700

    [MOD] Fixframe

--- Asset/Scripts/Fixframe.cs
+++ Asset/Scripts/Fixframe.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fixframe : MonoBehaviour {
+    // Use this for initialization
+    Bounds Temp;
+    SpriteRenderer sr;
 
-	// Use this for initialization
-	void Start () {
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+    bool IsChange = false;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        ChangeFrame();
+    }
+    void Update()
+    {
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+        else if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+            Screen.orientation = ScreenOrientation.LandscapeRight;
+    }
+    void ChangeFrame()
+    {
         Vector3 TempScale = transform.localScale;
 
         float Height = sr.bounds.size.y;
@@ -15,9 +32,9 @@ public class Fixframe : MonoBehaviour {
         float WorldHeight = Camera.main.orthographicSize * 2f;//10
         float WorldWidth = WorldHeight * Screen.width / Screen.height;
 
-        TempScale.y = WorldHeight/Height;
-        TempScale.x = WorldWidth/width;
+        TempScale.y = WorldHeight / Height;
+        TempScale.x = WorldWidth / width;
 
         transform.localScale = TempScale;
-	}	
+    }
 }
