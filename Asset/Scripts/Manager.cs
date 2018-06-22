5ea015faf87aef14251a22c82effd823cfae7ae2
KYKAROS\Kykaros <panadonoop3@gmail.com>
2018-06-22 22:52:59 +0700

    [MOD] Manager

--- Asset/Scripts/Manager.cs
+++ Asset/Scripts/Manager.cs
@@ -7,8 +7,10 @@ public class Manager : MonoBehaviour
     public Sprite Gameover;
     public Sprite Startgame;
     bool IsOpen = false;
+    public static bool FrameGameOver = false;
     public GameObject Again;
     public GameObject score;
+    int ChangeState = 5;
     // Use this for initialization
     void Start()
     {
@@ -31,6 +33,12 @@ public class Manager : MonoBehaviour
             //luu diem
             Score.SaveScore();
         }
+        if(Score.Point == ChangeState)
+        {
+            //SpawnColum.SetRepeatTime(SpawnColum.RepeatTime - 0.5f);
+            //ChangeState += 5;
+            //Debug.Log(ChangeState);
+        }
     }
     void GameOver()
     {
@@ -38,7 +46,7 @@ public class Manager : MonoBehaviour
         this.GetComponent<SpriteRenderer>().sprite = Gameover;
         Again.SetActive(true);
         score.SetActive(true);
-        Bird.Death = false;
+        FrameGameOver = true;
     }
     void StartGame()
     {
