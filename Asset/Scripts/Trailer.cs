2b3fa47161e9e651fdfe7d014d7ac5edad8e98e5
KYKAROS\Kykaros <panadonoop3@gmail.com>
2018-06-22 23:02:18 +0700

    [ADD] Trailer

---
+++ Asset/Scripts/Trailer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Trailer : MonoBehaviour {
+
+    // Use this for initialization
+    double time;
+    double currtime;
+    void Start()
+    {
+        time = GetComponent<VideoPlayer>().clip.length;
+        Debug.Log(time);
+    }
+    void Update()
+    {
+        currtime = gameObject.GetComponent<VideoPlayer>().time;
+        Debug.Log(currtime);
+        if (currtime >= time)
+            SceneManager.LoadScene(1);
+    }
+}
